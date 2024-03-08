64ff1a1230480df8362e56ef1c97d4395ef2ff5e
Aztol <laurent0499@gmail.com>
2024-03-08 08:06:02 +0100

    add of title and year research

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ import os
 # Set your Google Cloud credentials
 # Make sure to replace 'path/to/your/service-account-file.json' with the path to your service account key file
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\Laurent Sierro\Documents\Clef_Gcloud\kamboo-creek-415115-6445343d2370.json"
+#os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"C:\Users\Laurent Sierro\Documents\Clef_Gcloud\kamboo-creek-415115-6445343d2370.json"
 #os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/credentials.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"/Users/laurentsierro/Documents/bamboo-creek-415115-6445343d2370.json"
 # Initialize a BigQuery client
 client = bigquery.Client()
 
@@ -31,7 +32,7 @@ def fetch_languages():
     return sorted(unique_languages)
 
 # Function to fetch movies by genre
-def fetch_movies(genre, language, min_avg_rating):
+def fetch_movies(genre, language, min_avg_rating, title=None, start_year=None, end_year=None):
     # Modified base query to include JOIN and compute average rating
     query = """
         SELECT md.title, AVG(mr.rating) AS average_rating
@@ -45,6 +46,12 @@ def fetch_movies(genre, language, min_avg_rating):
     
     if language != 'Select a language':
         conditions.append(f"md.language = '{language}'")
+    
+    if title:
+        conditions.append(f"LOWER(md.title) LIKE LOWER('%{title}%')")
+    
+    if start_year and end_year:
+        conditions.append(f"md.release_year BETWEEN {start_year} AND {end_year}")
     
     # Combine WHERE conditions if any
     if conditions:
@@ -82,10 +89,14 @@ def main():
     rating_options = ['Select a rating', 0, 1, 2, 3, 4, 5]
     min_avg_rating = st.selectbox('Choose a minimum average rating', rating_options)
 
-    # Fetch and display movies
-    if st.button('Show Movies') and (genre != 'Select a genre' or language != 'Select a language' or min_avg_rating != 'Select a rating'):
+    title = st.text_input('Enter a movie title')
 
-        movies_list = fetch_movies(genre, language, min_avg_rating)
+    start_year = st.number_input('Enter a start year', min_value=1900, step=1, value=1900)
+    end_year = st.number_input('Enter an end year', min_value=1900, step=1, value=2022)
+
+    # Fetch and display movies
+    if st.button('Show Movies') and (genre != 'Select a genre' or language != 'Select a language' or min_avg_rating != 'Select a rating' or title or start_year <= end_year):
+        movies_list = fetch_movies(genre, language, min_avg_rating, title, start_year, end_year)
         movies_list = list(movies_list)
         if not movies_list:
             st.write('No movies found')
@@ -93,7 +104,7 @@ def main():
             for movie in movies_list:
                 st.write(movie.title)
     else:
-        st.write('Please select a genre, language, and minimum average rating and click on "Show Movies"')
+        st.write('Please select a genre, language, and minimum average rating, enter a movie title, and specify a valid release date range, then click on "Show Movies"')
 
 if __name__ == "__main__":
     main()
